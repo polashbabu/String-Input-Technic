d294a22956494f937aeda1ee478d2ee28ab4ae12
MD.Jahid Hassan <32840051+polashbabu@users.noreply.github.com>
2019-02-24 17:07:26 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace String_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now can write your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("My name is :"+ name);
+            Console.ReadKey();
+            
+        }
+    }
+}
